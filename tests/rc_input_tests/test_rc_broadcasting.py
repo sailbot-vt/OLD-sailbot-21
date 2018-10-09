df30a0d4bd2bcc3c7a92213d88cab5e3b3610d73
William Cabell <william16180@gmail.com>
2018-10-09 07:08:43 -0400

    Added test stubs for rc_broadcasting

---
+++ tests/rc_input_tests/test_rc_broadcasting.py
@@ -0,0 +1,21 @@
+import context
+import unittest
+
+from rc_input import rc_broadcasting
+
+
+class RCBroadcastingTests(unittest.TestCase):
+    """Tests methods in RCMessenger"""
+
+    def test_change_trim(self):
+        assert True
+
+    def test_move_rudder(self):
+        assert True
+
+    def test_change_mode(self):
+        assert True
+
+
+if __name__ == "__main__":
+    unittest.main()
