8c1df428b3fb0b3fecca4e7080f056f597f49a0c
William Cabell <william16180@gmail.com>
2018-10-09 07:15:25 -0400

    Added test stubs for rc_receiver_reader

---
+++ tests/rc_input_tests/test_rc_receiver_reader.py
@@ -0,0 +1,18 @@
+import context
+import unittest
+
+from rc_input import rc_receiver_reader
+
+
+class RCReceiverReaderTests(unittest.TestCase):
+    """Tests methods in RCMessenger"""
+
+    def test_run(self):
+        assert True
+
+    def test_read_input(self):
+        assert True
+
+
+if __name__ == "__main__":
+    unittest.main()
