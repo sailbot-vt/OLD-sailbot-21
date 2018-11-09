4c8c2d38127f47b53eb5a26359999c989d9aef23
William Cabell <william16180@gmail.com>
2018-11-09 13:09:37 -0500

    Created test stubs for msg system
    Build status also now reflects completion status

---
+++ tests/msg_tests/test_publish.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class TestPublish(unittest.TestCase):
+    """Tests publishing data to the message system."""
+
+    def setUp(self):
+        """Runs before each test method."""
+        pass
+
+    def test_publish(self):
+        """Tests the publish method."""
+        assert False
+
+    def tearDown(self):
+        """Runs after all test methods."""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
