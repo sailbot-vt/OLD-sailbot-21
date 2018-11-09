9c9429642608ab0aff0198188c7fa0229222de42
William Cabell <william16180@gmail.com>
2018-11-09 15:05:35 -0500

    Added placeholder parameter for boat config

--- src/rc_input/rc_input_thread.py
+++ src/rc_input/rc_input_thread.py
@@ -8,7 +8,11 @@ RC_READ_INTERVAL = 50 / 1000  # Scaled for milliseconds
 
 
 class RCInputThread(Thread):
-    def __init__(self):
+    """A separate thread to manage reading the RC inputs and broadcasting the data to the system.
+
+    Should accept multiple boat configurations, and should be general enough to allow for easy extension.
+    """
+    def __init__(self, config):
         """Builds a new RC input thread."""
         super().__init__()
 
