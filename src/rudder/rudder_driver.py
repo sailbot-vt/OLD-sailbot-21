42446c7097ef24040fd8e7a8c4c30b6198cf74f7
Tom Twomey <twomey@vt.edu>
2018-11-09 12:08:27 -0500

    Some fixes and prep for testing.

--- src/rudder/rudder_driver.py
+++ src/rudder/rudder_driver.py
@@ -7,8 +7,10 @@ Expected messgae to be object such that:
     -For TRIM_SAIL positive brings the winch in.
 """
 from threading import Thread
-from consumer import consumer
-import RudderServoController
+from src.msg_system.consumer import consumer
+from enum import Enum
+from src.rudder.rudder_servo_controller import RudderServoController
+import Adafruit_BBIO
 
 """ I don't know where to put these constants
 Most need to be determined by testing"""
@@ -20,15 +22,16 @@ angle_min = -180
 mechanical_advantage = 1
 
 class RudderThread(Thread):
-    """ Thead to subscribe and move the servo as such."""
+    """ Thread to subscribe and move the servo as such."""
 
     def run(self):
         """
         Entry point.
         Not sure if this will run multiple times
         """
+        pwm_pin = Adafruit_BBIO.PWM
         rudder_control = RudderServoController(pwm_pin, duty_min, duty_max, angle_min, angle_max, mechanical_advantage, pwm_lib)
-        subscriber = RudderConsumer()
+        subscriber = make_rudder_consumer(RudderConsumerType.Production)
         subscriber.register_to_consume_data("RCComand")
 
 
@@ -44,4 +47,41 @@ class RudderConsumer(consumer):
         """
         if not data.TURN_RUDDER is None:
             delta_rudder_angle = data.TURN_RUDDER
-            rudder_control.change_rudder_angle(delta_rudder_angle)
+            self.rudder_control.change_rudder_angle(delta_rudder_angle)
+
+class TestableRudderConsumer():
+    """
+    Mock consumer to feed artifical input to the rudder_control
+    """
+    def __init__(self):
+        """
+        Make a bunch of angles to turn.
+        """
+        self.delta_angles = [0, -0, 15, -15, 90, -90, 90, 90, -180, 90]
+
+    def register_to_consume_data(self, data):
+        """
+        Mock this method as it is the one that is called.
+        """
+        for angle in self.delta_angles:
+            self.rudder_control.change_rudder_angle(angle)
+
+class RudderConsumerType(Enum):
+    Testable = 0,
+    Production = 1
+
+
+def make_rudder_consumer(rudder_consumer_type):
+    """
+    Create a consumer that is real or mocked for the purpose of testing.
+
+    Keyword arguments:
+    rudder_consumer_type -- The type of consumer to create
+
+    Returns:
+    The correct type of consumer.
+    """
+    if rudder_consumer_type == RudderConsumerType.Testable:
+        return TestableRudderConsumer()
+
+    return RudderConsumer()
--- src/rudder/rudder_servo_controller.py
+++ src/rudder/rudder_servo_controller.py
@@ -14,17 +14,17 @@ class RudderServoController:
         pwm_pin -- The pin being used to control the servo.
         duty_min -- The duty to send the servo to the full left position
         duty_max -- The duty to send the servo to the full right position
-        angle_min -- The minimum allowed angle.
-        angle_max -- The maximum allowed angle.
+        angle_min -- The minimum allowed angle of servo.
+        angle_max -- The maximum allowed angle of servo.
         pwm_lib -- The Adafruit_BBIO PWM library or mock.
 
         Side effects:
         - Initializes instance variables
         - Sends the rudder to 0 rudder_angle
         """
-        servo = Servo(pwm_pin, duty_min, duty_max, angle_min, angle_max, pwm_lib)
+        self.servo = Servo(pwm_pin, duty_min, duty_max, angle_min, angle_max, pwm_lib)
         self.mechanical_advantage = mechanical_advantage
-        current_rudder_angle = 0
+        self.current_rudder_angle = 0
         self.rudder_turn_to(0)
 
 
@@ -48,6 +48,8 @@ class RudderServoController:
         and the max of the servo and the mechanical_advantage to determine
         possible angles.
 
+        Also limits the angle range to -90 and 90
+
         Keyword arguments:
         rudder_angle -- The desired angle of the rudder
 
@@ -58,6 +60,10 @@ class RudderServoController:
             return self.servo.angle_max / self.mechanical_advantage
         if (rudder_angle * self.mechanical_advantage < self.servo.angle_min):
             return self.servo.angle_min / self.mechanical_advantage
+        if (rudder_angle > 90):
+            return 90
+        if (rudder_angle < -90):
+            return -90
         return rudder_angle
 
 
@@ -75,7 +81,7 @@ class RudderServoController:
         """
         constrained_rudder_angle = self.constrain_rudder_angle(rudder_angle)
         self.servo.turn_to(self.rudder_angle_to_servo_angle(constrained_rudder_angle))
-        current_rudder_angle = constrained_rudder_angle
+        self.current_rudder_angle = constrained_rudder_angle
         return constrained_rudder_angle
 
 
