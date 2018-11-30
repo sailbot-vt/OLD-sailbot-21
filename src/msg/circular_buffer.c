1370db2b63aef65bb46ff636dfc70d84c33f7801
William Cabell <william16180@gmail.com>
2018-11-30 15:09:26 -0500

    Added circular buffer implementation

--- src/msg/circular_buffer.c
+++ src/msg/circular_buffer.c
@@ -3,3 +3,53 @@
 //
 
 #include "circular_buffer.h"
+
+
+// Functions
+
+CircularBuffer* init_circular_buffer() {
+    CircularBuffer* new_buffer = (CircularBuffer*)malloc(sizeof(CircularBuffer));
+    new_buffer->size = 0;
+    new_buffer->head = 0;
+    new_buffer->tail = 0;
+}
+
+
+void push(CircularBuffer* buffer, void* data, size_t data_size) {
+    Data* new_data = (Data*)malloc(sizeof(Data));
+    new_data->size = data_size;
+    new_data->data = data;
+
+    int next_index = buffer->size == 0 ? 1 : (buffer->head + 1) % MAX_BUFFER_SIZE;
+
+    buffer->data[next_index] = *new_data;
+
+    buffer->head = next_index;
+    buffer->tail = (buffer->head - buffer->size + 1) % MAX_BUFFER_SIZE;
+
+    if (buffer->size < MAX_BUFFER_SIZE) {
+        buffer->size++;
+    }
+}
+
+
+Data* get_element(CircularBuffer* buffer, Element elem) {
+    if (elem.index <= buffer->head && buffer->revolutions != elem.revolution) {
+        return (Data*)NULL;
+    }
+
+    return &buffer->data[elem.index];
+}
+
+
+void empty_buffer(CircularBuffer* buffer) {
+    buffer->size = 0;
+    buffer->head = 0;
+    buffer->tail = 0;
+}
+
+
+void destroy_circular_buffer(CircularBuffer** buffer) {
+    free(*buffer);
+    *buffer = (CircularBuffer*)NULL;
+}
--- src/msg/circular_buffer.h
+++ src/msg/circular_buffer.h
@@ -5,32 +5,81 @@
 #ifndef CircularBuffer_h
 #define CircularBuffer_h
 
-#define MAX_QUEUE_SIZE 256
+
+#include <stdlib.h>
+
+
+#define MAX_BUFFER_SIZE 256
 
 
 // Structs
 
-typedef struct CircularBuffer {
-    unsigned int head;
-    unsigned int tail;
-
-} CircularBuffer;
-
 typedef struct Element {
-    uint64_t iteration;
+    uint64_t revolution;
     int index;
 } Element;
+
 
 typedef struct Data {
     size_t size;
     void* data;
-};
+} Data;
+
+
+typedef struct CircularBuffer {
+    int size;
+    int head;
+    int tail;
+    uint64_t revolutions;
+    Data data[MAX_BUFFER_SIZE];
+} CircularBuffer;
 
 
 // Functions
 
-void push(CircularBuffer*, void* data, size_t size);
-Data get_element(CircularBuffer*, Element);
-void emptyQueue(CircularBuffer*)
+/*
+ * Creates a new CircularBuffer.
+ *
+ * Returns:
+ * A new CircularBuffer struct initialized to be empty.
+ */
+CircularBuffer* init_circular_buffer(void);
+
+/*
+ * Adds a new Data item to the buffer.
+ *
+ * WILL OVERRIDE DATA ALREADY IN THE BUFFER.
+ *
+ *
+ * Keyword arguments:
+ * buffer -- The CircularBuffer to which to add the data.
+ * data -- A pointer to the data.
+ * data_size -- The number of bytes in the data.
+ */
+void push(CircularBuffer* buffer, void* data, size_t data_size);
+
+/*
+ * Gets an element from the buffer.
+ *
+ * Returns NULL and executes the callback function if the requested data has been overridden.
+ *
+ * Keyword arguments:
+ * buffer -- The buffer from which to attempt to get the data.
+ * elem -- An element struct specifying which index and iteration of the data to get.
+ *
+ * Returns:
+ * The Data struct containing the data or NULL.
+ */
+Data* get_element(CircularBuffer* buffer, Element elem);
+
+/*
+ *
+ */
+void empty_buffer(CircularBuffer *buffer);
+
+/*
+ *
+ */
+void destroy_circular_buffer(CircularBuffer** buffer);
 
 #endif /* CircularBuffer_h */
