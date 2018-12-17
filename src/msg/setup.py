1a43f1fbea84673803df3fdb8359e3b464cdfc5e
William Cabell <william16180@gmail.com>
2018-12-17 13:26:57 -0500

    Fixed test build

--- src/msg/setup.py
+++ src/msg/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize(
-        [Extension("cython_subscriber", ["cython_subscriber.pyx"]),
-        Extension("cython_publisher", ["cython_publisher.pyx"])]
-))
+setup(ext_modules=cythonize([
+        Extension("cython_subscriber", ["cython_subscriber.pyx"], extra_compile_args=['-std=c11']),
+        Extension("cython_publisher", ["cython_publisher.pyx"], extra_compile_args=['-std=c11'])
+]))
