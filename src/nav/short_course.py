bd3927c101052bc3d4f4e6e19e36e0c17a66ced1
William Cabell <william16180@gmail.com>
2019-01-04 12:37:25 -0500

    Outlined tack points algorithm

--- src/nav/short_course.py
+++ src/nav/short_course.py
@@ -1,6 +1,8 @@
-from src.utils import *
-from src.world import World
-from src.boat import Boat
+import numpy as np
+import scipy.optimize
+
+
+PATH_RESOLUTION = 100
 
 
 def generate_path(
@@ -20,4 +22,52 @@ def generate_path(
     Returns:
     An array of Vec2s along the routed path
     """
+
+    # Choose n tack points
+    for n_tacks in range(find_max_reasonable_tacks()):
+        initial_tack_points = guess_tack_points()
+
+        # Generate a corresponding path
+
+        # Minimize time for the path
+        scipy.optimize.minimize(time_for_path, initial_tack_points)
+
+    # Find best path
+
+    # Unflatten the path and return it
     pass
+
+
+def time_for_path(tack_points):
+    """Estimates the total time for a path.
+
+    The tack points are handled as a flat array for faster optimization.
+
+    Keyword arguments:
+    tack_points -- A flat array of points in 2d.
+
+    Returns:
+    An estimate of the time for the path defined by the tack points.
+    """
+    path = build_path_from_tack_points(tack_points)
+
+    pass
+
+
+def build_path_from_tack_points(tack_points):
+    """Interpolates points in between tack points."""
+
+    return np.array([])
+
+
+def unflatten_path(flat_path):
+    """Generates a path of Vec2s from a flat path."""
+    pass
+
+
+def find_max_reasonable_tacks():
+    return 4
+
+
+def guess_tack_points():
+    return np.array([])
--- src/nav/world/world.py
+++ src/nav/world/world.py
@@ -1,5 +1,3 @@
-import src.world.wind
-
 class World:
     """State object for the current """
     def __init__(self):
