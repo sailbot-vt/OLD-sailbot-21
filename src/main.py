625fe0b055ea59e5d266be811adf26fd6ad9af0a
William Cabell <william16180@gmail.com>
2019-01-25 21:20:36 -0500

    Implemented main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
+import src.rc_input.rc_input_thread as rc
+import src.sail.sail_thread as sail
+import src.rudder.rudder_thread as rudder
+
+
 def main():
     """Runs the program."""
-    pass
+    rc_thread = rc.RCInputThread()
+    sail_thread = sail.SailThread()
+    rudder_thread = rudder.RudderThread()
+
+    rudder_thread.start()
+    sail_thread.start()
+    rc_thread.start()
 
 
 if __name__ == "__main__":
