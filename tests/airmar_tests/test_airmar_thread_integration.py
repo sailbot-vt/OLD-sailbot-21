8c229291668de1a733aac226eaea64d55835d4f9
evanq123 <evanq123@gmail.com>
2019-02-22 14:27:04 -0500

    added airmar test cases

--- tests/airmar_tests/test_airmar_thread_integration.py
+++ tests/airmar_tests/test_airmar_thread_integration.py
@@ -16,6 +16,7 @@ class AirmarThreadTests(unittest.TestCase):
         serial.Serial.open = MagicMock(name='serial.Serial.open')
         serial.Serial.close = MagicMock(name='serial.Serial.close')
         serial.Serial.inWaiting = MagicMock(name='serial.Serial.inWaiting')
+        serial.Serial.read = MagicMock(name='serial.Serial.read')
         Adafruit_BBIO.UART.setup = MagicMock(name='Adafruit_BBIO.UART.setup')
 
         self.airmar_input_thread = AirmarInputThread(
@@ -23,12 +24,27 @@ class AirmarThreadTests(unittest.TestCase):
             mock_port=serial,
             broadcaster_type=AirmarBroadcasterType.Testable)
 
-    @patch('src.airmar.airmar_broadcaster.pub', autospec=True)
-    def test_none(self, mock_pub):
-        """ Tests that nothing is broadcasted when nothing is read. """
-        serial.Serial.read = MagicMock(name='serial.Serial.read',
-                                       return_value="")
+        self.broadcaster = self.airmar_input_thread.broadcaster
 
+    def test_none(self):
+        """ Tests that nothing is broadcasted when nothing is read. """
         self.airmar_input_thread.receiver.send_airmar_data()
 
-        mock_pub.sendMessage.assert_not_called()
+        assert len(self.broadcaster.wind_speeds) == 0
+        assert len(self.broadcaster.wind_heads) == 0
+        assert len(self.broadcaster.boat_lats) == 0
+        assert len(self.broadcaster.boat_longs) == 0
+        assert len(self.broadcaster.boat_heads) == 0
+        assert len(self.broadcaster.boat_speeds) == 0
+
+    def test_wind(self):
+        """ Tests that wind speed data was recorded """
+        pass
+
+    def test_boat_gps(self):
+        """ Tests that boat latitudes and longitudes were recorded """
+        pass
+    
+    def test_boat_move(self):
+        """ Tests that boat heading and speed were recorded """
+        pass
