0129b8f2f00604bbfe9bc18fb5ca6f71c007a34e
evanq123 <evanq123@gmail.com>
2019-03-06 16:44:07 -0500

    Added dtbo variable links to setup script

--- src/hardware/setup-uart.sh
+++ src/hardware/setup-uart.sh
@@ -1,5 +1,11 @@
  #! /bin/sh cd /sys/devices/platform/bone_capemgr File=slots if grep -q "Override Board Name,00A0,Override Manuf,univ-emmc" "$File";  then    
     cd
+    ###Overide capes with eeprom
+    uboot_overlay_addr0=/lib/firmware/BB-UART1-00A0.dtbo
+    uboot_overlay_addr1=/lib/firmware/BB-UART2-00A0.dtbo
+    uboot_overlay_addr2=/lib/firmware/BB-UART4-00A0.dtbo
+    uboot_overlay_addr3=/lib/firmware/BB-UART5-00A0.dtbo
+    
     echo "\n Pin configuration available"
     echo "\n UART 4 configuration p9.11 and p9.13"
     sudo config-pin P9.11 uart
