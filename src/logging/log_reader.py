5b297bb4181cadeda236f7229d30fcc43e6adefe
ryansingman <ryanjs@vt.edu>
2019-03-20 10:44:57 -0400

    Added comments to log_reader and logger

--- src/logging/log_reader.py
+++ src/logging/log_reader.py
@@ -3,33 +3,60 @@ import pdb
 from pprint import pprint as pp
 
 class logReader():
-
+    """
+    Class that allows for log readback
+    """
     def __init__(self, infile):
+        """
+        Initialization of reader
+        Reads header and config files
+
+        Keyword arguments:
+        infile -- File path to log file to be read
+        """ 
         self.infile = infile
         self.lineno = 0
         self.read_header()
         self.read_configs()
 
     def read_header(self):
+        """
+        Reads header and stores as dictionary
+        """
         with open(self.infile, 'r') as log_file:
             self.header = json.loads(log_file.readline())
         self.lineno += 1
 
     def read_configs(self):
+        """
+        Reads config files and stores as dictionary
+        """
         num_config_files = self.header['num_config_files'] 
         self.config_dict = {}
         for n in range(num_config_files):
             with open(self.infile, 'r') as log_file:
                 temp_config_dict = json.loads(log_file.readline())
+            self.lineno += 1
             self.config_dict.update(temp_config_dict)
 
     def read_log(self):
+        """
+        Reads log line by line as a generator
+        Create generator by calling:
+            example_generator = exampleLogReader.read_log()
+        Obtain logs one by one by calling:
+            exampleLog = example_generator.__next__()
+        
+        Returns:
+        A log with datetime, pin number, and a message
+        """
         with open(self.infile, 'r') as log_file:
             for line in log_file.readlines():
                 yield json.loads(line)
+                self.lineno += 1
             
 
-
+#Brief test
 if __name__ == '__main__':
     ex_logReader = logReader('logging/dir_log/2019_03_19_1.log')
 
@@ -39,5 +66,3 @@ if __name__ == '__main__':
 
     while 1:
         pp(msg_generator.__next__())
-
-    pdb.set_trace()
--- src/logging/logger.py
+++ src/logging/logger.py
@@ -6,16 +6,25 @@ import glob
 import pdb
 
 class Logger():
+    """
+    Class that allows processes to log messages in log files in JSON format
+    """
     def __init__(self):
-        self.outfile_name = self._get_file_name()
-        yml_file_list = self._find_config_files()
+        self.outfile_name = self._get_file_name()       #Gets default log file name (SHOULD CHANGE DIRECTORY THAT LOGS GO IN
+        yml_file_list = self._find_config_files()       #Finds all config files in subdirectories
         header_dict = {'file_name' : self.outfile_name, 'num_config_files': len(yml_file_list)}
         with open(self.outfile_name, 'w') as outfile:
-            json.dump(header_dict, outfile)
+            json.dump(header_dict, outfile)             #Write header to log file
             outfile.write('\n')
-        self._record_config_files(yml_file_list)
+        self._record_config_files(yml_file_list)        #Write config files to log to track config values for each process
 
     def _find_config_files(self):
+        """
+        Finds all config.yml files in subdirectories
+
+        Returns:
+        A list of config.yml file paths
+        """
         config_file_list = []
         for directory in os.listdir('./'):
             for config_file in glob.iglob('./' + directory + '**/*.yml', recursive=True):
@@ -24,6 +33,12 @@ class Logger():
         return config_file_list
   
     def _record_config_files(self,yml_file_list):
+        """
+        Loads config files from yml format and dumps them as JSON in the log file
+
+        Keyword arguments:
+        yml_file_list --  List of config.yml file paths
+        """
         for config_file in yml_file_list:
             with open(config_file, 'r') as infile:
                 config_dict = yaml.safe_load(infile)
@@ -34,6 +49,13 @@ class Logger():
 
 
     def write_msg(self, pin_no, msg):
+        """
+        Writes a log given data from process
+
+        Keyword arguments:
+        pin_no -- Pin number that event occurs at
+        msg -- Message to be recorded (data on pin)
+        """
 
         datetime_str = self._get_datetime_str()
         
@@ -45,20 +67,33 @@ class Logger():
 
 
     def _get_datetime_str(self):
+        """
+        Finds formatted current date and time
+
+        Returns:
+        A string formatted date and time
+        """
         return datetime.datetime.now().strftime('%Y-%m-%d // %H:%M:%S')
 
     def _get_file_name(self):
+        """
+        Finds a filename for the output log file
+
+        Returns:
+        Filename for the log file
+        """
         current_date = datetime.datetime.now().strftime('%Y_%m_%d')
         n = 1
         temp_name = 'logging/dir_log/' + current_date + '_%d.log' % n
-        while os.path.isfile(temp_name):
+        while os.path.isfile(temp_name):                                    #Loops until open file name is found
             n += 1
             temp_name = 'logging/dir_log/' + current_date + '_%d.log' % n
         return temp_name
     
 
+#Brief test
 if __name__ == '__main__':
-    
+ 
     ex_logger = Logger()
 
     ex_logger.write_msg(4, 'abcdefg testmsg') 
