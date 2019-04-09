ddd24403d4bf6b7083823b7dfe456ad4e514ad70
wlans4 <wlans4@vt.edu>
2019-04-08 20:34:58 -0400

    depth map done

--- src/buoy_detection/image_capture.py
+++ src/buoy_detection/image_capture.py
@@ -1,11 +1,18 @@
 import cv2
+import glob
+import os
 
-LEFT_PATH = "/home/wlans4/PycharmProjects/sailbot-19/src/buoy_detection/buoy_detection/LEFT/{:01d}.png"
-RIGHT_PATH = "/home/wlans4/PycharmProjects/sailbot-19/src/buoy_detection/buoy_detection/RIGHT{:01d}.png"
-
+CHESSBOARD_CALIBRATION_SIZE = (6,9)
+IMAGES_TO_TAKE = 100
+LEFT_PATH = "/home/wlans4/PycharmProjects/sailbot-19/src/buoy_detection/buoy_detection/LEFT/"
+RIGHT_PATH = "/home/wlans4/PycharmProjects/sailbot-19/src/buoy_detection/buoy_detection/RIGHT/"
 left = cv2.VideoCapture(0)
 right = cv2.VideoCapture(1)
 
+
+
+LEFT = LEFT_PATH + "{:01d}.png"
+RIGHT = RIGHT_PATH + "{:01d}.png"
 frame_count = 0
 
 while True:
@@ -19,14 +26,15 @@ while True:
     cv2.imshow('right', right_frame)
     key = cv2.waitKey(1)
     if key%256 == 32:
-        cv2.imwrite(LEFT_PATH.format(frame_count), left_frame)
-        cv2.imwrite(RIGHT_PATH.format(frame_count), right_frame)
+        cv2.imwrite(LEFT.format(frame_count), left_frame)
+        cv2.imwrite(RIGHT.format(frame_count), right_frame)
         frame_count += 1
     elif key%256 == 27:
         break
-    if frame_count >= 25:
+    if frame_count >= IMAGES_TO_TAKE:
         break
 
 left.release()
 right.release()
 cv2.destroyAllWindows()
+
