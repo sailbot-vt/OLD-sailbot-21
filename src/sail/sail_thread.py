5178797ac8e6ab58da892f7fa604d80ba164eb8a
root <root@beaglebone.localdomain>
2019-04-30 03:46:36 +0000

    Fixed pypubsub syntax cont.

--- src/sail/sail_thread.py
+++ src/sail/sail_thread.py
@@ -14,7 +14,7 @@ class SailThread(Thread):
         super().__init__()
         servo = Servo(build_pin_from_config(), read_servo_config())
         self.mainsheet = Mainsheet(servo, read_mainsheet_config())
-        pub.subscribe("set trim", self.received_trim_command)
+        pub.subscribe(self.received_trim_command, "set trim")
 
     def received_trim_command(self, degrees_in):
         self.mainsheet.trim_in_by(degrees_in)
