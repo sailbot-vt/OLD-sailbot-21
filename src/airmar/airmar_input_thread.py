3d0b9c3f1c0f8acfbad8c58f7b751ab972965381
evanq123 <evanq123@gmail.com>
2019-05-09 19:55:33 -0400

    Updated airmar thread

--- src/airmar/airmar_input_thread.py
+++ src/airmar/airmar_input_thread.py
@@ -1,7 +1,7 @@
 from threading import Thread
 from time import sleep
 
-from src.airmar.config_reader import read_pin_config, read_interval, read_port_config, read_sentences
+from src.airmar.config_reader import read_pin_config, read_interval, read_port_config, read_ids
 from src.airmar.airmar_receiver import AirmarReceiver
 from src.airmar.airmar_broadcaster import make_broadcaster, AirmarBroadcasterType
 
@@ -12,17 +12,17 @@ class AirmarInputThread(Thread):
     def __init__(self, mock_bbio=None, mock_port=None, broadcaster_type=None):
         """Builds a new airmar input thread."""
         super().__init__()
-        if broadcaster_type is None:
-            # Default broadcaster:
-            broadcaster_type = AirmarBroadcasterType.Messenger
 
+        if broadcaster_type is None:
+            broadcaster_type = AirmarBroadcasterType.Messenger
         broadcaster = make_broadcaster(broadcaster_type=broadcaster_type)
+        
         pin = read_pin_config(mock_bbio=mock_bbio)
         port = read_port_config(mock_port=mock_port)
-        sentences = read_sentences()
+        ids = read_ids()
 
         self.receiver = AirmarReceiver(
-            broadcaster=broadcaster, sentences=sentences, pin=pin, port=port)
+            broadcaster=broadcaster, ids=ids, pin=pin, port=port)
 
         self.keep_reading = True
         self.read_interval = read_interval()
@@ -34,7 +34,6 @@ class AirmarInputThread(Thread):
         -- self.keep_reading : True, receiver start, if not started/stopped
         -- self.keep_reading : False, receiver stop
         """
-        self.keep_reading = True
         self.keep_reading = True
         while self.keep_reading:
             if not self.receiver.is_running:
