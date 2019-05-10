a6e8d93f033f5b255e2cb1cd42cc091160720311
evanq123 <evanq123@gmail.com>
2019-05-09 20:03:07 -0400

    Updated airmar receiver

--- src/airmar/airmar_receiver.py
+++ src/airmar/airmar_receiver.py
@@ -1,12 +1,13 @@
 import re
 
+from src.airmar.nmeaparser.nmea import nmea
 from src.airmar.airmar_processor import AirmarProcessor
 
 
 class AirmarReceiver:
     """Defines an Airmar receiver that sends data to a processor."""
 
-    def __init__(self, broadcaster, sentences, pin, port):
+    def __init__(self, broadcaster, ids, pin, port):
         """Initializes a new airmar receiver.
 
         Keyword arguments:
@@ -15,33 +16,40 @@ class AirmarReceiver:
         Returns:
         A new Airmar Receiver
         """
-        self.sentences = sentences
+        self.parser = nmea()
+        self.ids = ids
         self.is_running = False
-        self.remaining_input = ""
         self.uart_pin = pin
         self.port = port
         self.processor = AirmarProcessor(broadcaster=broadcaster)
 
     def start(self):
-        """ Sets up uart pin and open port to start listening."""
+        """ Sets up uart pin and open port to start listening. 
+        Enables sentences specified by ids field to airmar serial port."""
         self.uart_pin.setup()
+        # Close port before open needed.
+        self.port.close()
         self.port.open()
-        self.port.write(b"$PAMTC,EN,ALL,0,1,,*00\r\n")
-        # TODO Need to implement check sum
-        for sentence_id in self.sentences:
-            sentence = b"{}".format(sentence_id)
-            self.port.write(sentence)
+        # Resumes sentence transmition
+        self.port.write(b"{}".format(self.parser.power(resume=1)))
+        # Disable all sentence transmitions first.
+        self.port.write(b"{}".format(self.parser.toggle(enable=0)))
+        for sid in self.ids:
+            # Enables sentences specified by config
+            self.port.write(b"{}".format(self.parser.toggle(sid)))
         self.is_running = True
 
     def send_airmar_data(self):
-        """ Sends NMEASentence object to airmar processor to broadcast airmar data."""
-        nmea_obj = self._parse_msg()
-
-        if nmea_obj is not None:
-            self.processor.update_airmar_data(nmea=nmea_obj)
+        """ Sends nmea sentence from serial port to processor to broadcast data """
+        data = self.parser.parse(self.port.read_line(terminator='\r\n'))
+        # sentence type can be determined by first element of data.
+        if data is not None:
+            self.processor.update_airmar_data(nmea=data)
 
     def stop(self):
         """ Stops the pin and port """
+        # Suspends sentences.
+        self.port.write(b"{}".format(self.parser.power(resume=0)))
         self.port.close()
         self.uart_pin.cleanup()
         self.is_running = False
