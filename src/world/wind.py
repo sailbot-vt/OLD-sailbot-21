39c6427dce3d88b1c8860d49a2a99ac05d137fa1
William Cabell <william16180@gmail.com>
2019-05-21 10:16:02 -0400

    Added wind API

--- src/world/wind.py
+++ src/world/wind.py
@@ -1,30 +1,59 @@
-import math
+from pubsub import pub
+
 from src.utils.vec import Vec2
 
 
-def true_wind(current_velocity):
-    """Calculates the true wind from estimated apparent wind
+class Wind:
+    """A unified API for all wind data"""
 
-    Keyword arguments:
-    apparent_wind -- The apparent wind as a Vec2
-    current_velocity -- The velocity as a Vec2
-    t -- The time at which to estimate the true wind
+    @property
+    def true_wind(self):
+        """Gets the latest true wind reading
 
-    Returns:
-    A Vec2 of the true wind
-    """
-    a = apparent_wind()  # Apparent wind
-    v = current_velocity  # Current velocity
-    awa = a.angle()  # Apparent wind angle
-    return math.sqrt(a ** 2
-                     + v ** 2
-                     - 2 * a * v * math.cos(awa))
+        Returns:
+        A Vec2 of the true wind
+        """
+        return self._true_wind
 
+    @property
+    def apparent_wind(self):
+        """Gets the latest apparent wind reading
 
-def apparent_wind():
-    """Calculates the apparent wind
+        Returns:
+        A Vec2 of the apparent wind
+        """
+        return self._apparent_wind
 
-    Returns:
-    A Vec2 of the apparent wind
-    """
-    return Vec2(0, 0)
+    def __init__(self):
+        """Builds a new wind object"""
+        self.true_wind_speed = 0
+        self.true_wind_angle = 0
+        self.apparent_wind_speed = 0
+        self.apparent_wind_angle = 0
+        self._true_wind = Vec2(0, 0)
+        self._apparent_wind = Vec2(0, 0)
+
+        pub.subscribe(self.update_true_wind_angle, "wind angle true")
+        pub.subscribe(self.update_true_wind_speed, "wind speed true")
+        pub.subscribe(self.update_apparent_wind_angle, "wind angle apparent")
+        pub.subscribe(self.update_apparent_wind_speed, "wind speed apparent")
+
+    def update_true_wind_angle(self, angle):
+        """Updates the true wind angle"""
+        self.true_wind_angle = angle
+        self._true_wind = Vec2.build_from(self.true_wind_speed, angle)
+
+    def update_true_wind_speed(self, speed):
+        """Updates the true wind speed"""
+        self.true_wind_speed = speed
+        self._true_wind = Vec2.build_from(speed, self.true_wind_angle)
+
+    def update_apparent_wind_angle(self, angle):
+        """Updates the apparent wind angle"""
+        self.apparent_wind_angle = angle
+        self._apparent_wind = Vec2.build_from(self.apparent_wind_speed, angle)
+
+    def update_apparent_wind_speed(self, speed):
+        """Updates the apparent wind speed"""
+        self.apparent_wind_speed = speed
+        self._apparent_wind = Vec2.build_from(speed, self.apparent_wind_angle)
