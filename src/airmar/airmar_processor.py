d89a3661db929601055c92a23fa836af7f94f3ea
evanq126 <evanq126@vt.edu>
2019-11-28 16:48:45 -0500

    Changed initial values in processor map

--- src/airmar/airmar_processor.py
+++ src/airmar/airmar_processor.py
@@ -7,15 +7,16 @@ class AirmarProcessor:
     def __init__(self, broadcaster, parser):
         self.parser = parser
         self.broadcaster = broadcaster
+
         self.data = {
-            "wind speed apparent" : None,
-            "wind angle apparent" : None,
-            "wind speed true": None,
-            "wind angle true": None,
-            "boat latitude": None,
-            "boat longitude": None,
-            "boat heading" : None,
-            "boat speed": None
+            "wind speed apparent" : 0,
+            "wind angle apparent" : 0,
+            "wind speed true": 0,
+            "wind angle true": 0,
+            "boat latitude": 0,
+            "boat longitude": 0,
+            "boat heading" : 0,
+            "boat speed": 0
         }
         self.raw = {}
 
@@ -25,30 +26,30 @@ class AirmarProcessor:
             return
         self.parser.update_data(data=self.raw, fields=nmea)
 
-        # TODO Update list with more interfaces
-        if sid in ["WIVWT", "WIVWR"]:
-            self._update_wind(sid)
-        elif sid in ["GPGGA"]:
+        if sid == "WIVWR":
+            self._update_wind(sid=sid, 
+                speed_key="wind speed apparent",
+                angle_key="wind angle apparent")
+        if sid == "WIVWT":
+            self._update_wind(sid=sid, 
+                speed_key="wind speed true",
+                angle_key="wind angle true")
+        elif sid == "GPGGA":
             self._update_boat_gps(sid)
-        elif sid in ["GPVTG"]:
+        elif sid == "GPVTG":
             self._update_boat_speed(sid)
-        
+        # Other updated processed data goes below
+
         # Broadcasts processed data via broadcaster
         self.broadcaster.update_dictionary(data=self.data)
 
-    def _update_wind(self, sid):
-        speed_key = "wind speed apparent"
-        angle_key = "wind angle apparent"
-        if sid is "WIVWT":
-            # Update true wind values instead
-            speed_key = "wind speed true"
-            angle_key = "wind angle true"
-        # Set old values = new values if no old values
-        if self.data[angle_key] is None:
-            self.data[angle_key] = float(self.raw[sid]["wind_angle_degree"]) % 360
-        if self.data[speed_key] is None:
-            self.data[speed_key] = float(self.raw[sid]["wind_speed_mps"])
 
+
+# --------------------  PROCESSED DATA ENTRY --------------------
+    def _update_wind(self, sid, speed_key, angle_key):
+        # update speed and angle for given sid
+        # Note, at start old values = 0, might skew values
+        # TODO Test above if values are skewed.
         self.data[speed_key], self.data[angle_key] = self._scale_avg_polar_coords(
             old_magnitude=self.data[speed_key],
             old_angle=self.data[angle_key],
@@ -56,6 +57,18 @@ class AirmarProcessor:
             new_angle=float(self.raw[sid]["wind_angle_degree"]) % 360
         )
 
+    def _update_boat_gps(self, sid):
+        # Update boat latitude and longitude
+        self.data["boat latitude"] = float(self.raw[sid]["latitude"])
+        self.data["boat longitude"] = float(self.raw[sid]["longitude"])
+
+    def _update_boat_speed(self, sid):
+        # Update boat speed and heading
+        self.data["boat speed"] = float(self.raw[sid]["speed_over_ground_kph"])
+        self.data["boat heading"] = float(self.raw[sid]["course_over_ground_true"]) % 360
+
+
+# -------------------- AIRMAR SPECIFIC CALCULATIONS --------------------
     def _scale_avg_polar_coords(self, old_magnitude, old_angle, new_magnitude, new_angle):
         # Convert to radians
         new_angle = math.radians(new_angle)
@@ -72,15 +85,5 @@ class AirmarProcessor:
         x = old_x * (1 - weight) + new_x * weight
         y = old_y * (1 - weight) + new_y * weight
 
+        # Convert to degrees, returns tuple (speed, angle)
         return math.sqrt(x*x + y*y), math.degrees(math.atan2(x, y)) % 360
-
-    def _update_boat_gps(self, sid):
-        if sid in self.raw:
-            self.data["boat latitude"] = float(self.raw[sid]["latitude"])
-            self.data["boat longitude"] = float(self.raw[sid]["longitude"])
-
-    def _update_boat_speed(self, sid):
-        if sid in self.raw:
-            self.data["boat speed"] = float(self.raw[sid]["speed_over_ground_kph"])
-            self.data["boat heading"] = float(self.raw[sid]["course_over_ground_true"]) % 360
-    
