0f0dab677a7e4a22e16234ae7dca3e3d2d41c24a
evanq126 <evanq126@vt.edu>
2019-11-28 23:01:43 -0500

    Fully tested nmea parsing

--- src/airmar/airmar_input_thread.py
+++ src/airmar/airmar_input_thread.py
@@ -9,14 +9,10 @@ from src.broadcaster.broadcaster import make_broadcaster, BroadcasterType
 class AirmarInputThread(Thread):
     """A separate thread to manage reading the airmar inputs."""
 
-    def __init__(self, mock_bbio=None, mock_port=None, broadcaster_type=None, filename=None):
+    def __init__(self, mock_bbio=None, mock_port=None, 
+        broadcaster_type=BroadcasterType.Messenger, filename=None):
         """Builds a new airmar input thread."""
         super().__init__()
-
-        # Make broadcaster
-        if broadcaster_type is None:
-            # pubsub by default
-            broadcaster_type = BroadcasterType.Messenger
 
         # broadcaster as public attribute
         self.broadcaster = make_broadcaster(
@@ -24,19 +20,14 @@ class AirmarInputThread(Thread):
         
         # make receiver
         self.receiver = AirmarReceiver(
-                broadcaster=self.broadcaster, mock_bbio=mock_bbio, mock_port=mock_port)
+                broadcaster=self.broadcaster, 
+                mock_bbio=mock_bbio, mock_port=mock_port)
 
-        self.keep_reading = True
         self.read_interval = read_interval()
 
     def run(self):
-        """Starts a regular read interval.
-        Side effects:
-        -- self.keep_reading : True, receiver start, if not started/stopped
-        -- self.keep_reading : False, receiver stop
-        """
-        self.keep_reading = True
-        while self.keep_reading:
+        """Starts a regular read interval."""
+        while self.is_alive():
             if not self.receiver.is_running:
                 self.receiver.start()
             try:
@@ -45,8 +36,5 @@ class AirmarInputThread(Thread):
                 continue
             sleep(self.read_interval)
         else:
+            # cleanup on thread exit.
             self.receiver.stop()
-
-    def stop(self):
-        """ Pauses current thread without killing it. """
-        self.keep_reading = False
