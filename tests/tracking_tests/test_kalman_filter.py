fixed scaling problem in transition matrix, added test for predict method

tests/tracking_tests/test_kalman_filter.py
@@ -32,7 +32,7 @@ class KalmanFilterTests(unittest.TestCase):
         truth_state_trans[0, 2] = 1
         truth_state_trans[1, 3] = 1
         self.kalman.last_time_changed = 0
-        mock_time_in_millis.return_value = 1
+        mock_time_in_millis.return_value = 1000
 
         self.kalman._update_trans_matrix()
         np.testing.assert_allclose(self.kalman.state_trans, truth_state_trans)
@@ -42,13 +42,67 @@ class KalmanFilterTests(unittest.TestCase):
         truth_state_trans[1, 3] = 2
 
         for n in range(3, 7, 2):
-            mock_time_in_millis.return_value = n
+            mock_time_in_millis.return_value = (n * 1000)
             self.kalman._update_trans_matrix()
 
             np.testing.assert_allclose(self.kalman.state_trans, truth_state_trans)
 
     def test_update(self):
         """Tests update method of kalman filter"""
+        # Testing methodology:
+        #   check if correct function calls are made with correct argument
+        #   check if kalman filter results correctly
 
-    def test_predict(self):
+    @patch('src.tracking.kalman_filter.KalmanFilter._update_trans_matrix')
+    def test_predict(self, mock_update_trans):
         """Tests predict method of kalman filter"""
+        # Testing methodology:
+        #   check if correct function calls are made with correct arguments
+        #   check if kalman filter results correctly
+        
+        # check if correct functions calls are made with correct arguments
+        with patch('src.tracking.kalman_filter.kalman.predict') as mock_predict:
+
+            # mock return values for predict
+            state = self.kalman.state
+            covar = self.kalman.covar
+            mock_predict.return_value = (state, covar)
+            
+            # call predict
+            self.kalman.predict()
+
+            # check for proper behavior
+            mock_update_trans.assert_called_once_with()
+            mock_predict.assert_called_once_with(x=self.kalman.state, P=self.kalman.covar, F=self.kalman.state_trans, Q=0)
+
+        # check if kalman filter results  correctly
+
+        # set up transition matrix
+        self.kalman.state_trans[0, 2] = 1
+        self.kalman.state_trans[1, 3] = 1
+
+        # set up state values, expected results
+        states = np.array([[0, 0, 5, -5],
+                           [1, 1, -1, -1],
+                           [-2, 0, 0, 3],
+                           [-3, -5, 2, 2]])
+
+        predicted_states = np.array([[5, -5, 5, -5],
+                                     [0, 0, -1, -1],
+                                     [-2, 3, 0, 3],
+                                     [-1, -3, 2, 2]])
+
+        # get covar matrix
+        covar = self.kalman.covar
+        
+        # loop thru different values for state
+        for ii in range(np.size(states, 0)):
+            # set kalman state
+            self.kalman.state = states[ii]
+
+            # call predict
+            self.kalman.predict()
+        
+            # check if predicted state is close to expected
+            predicted_state = predicted_states[ii]
+            np.testing.assert_allclose(predicted_state, self.kalman.state)