5fb4cc83f1ffac6a455c75a397c7a21a3ac6e21f
Ryan Singman <ryanjs@vt.edu>
2019-12-29 11:59:26 -0500

    Base event class filled out

--- src/events/base_event.py
+++ src/events/base_event.py
@@ -1,6 +1,12 @@
 from abc import ABC, abstractmethod
 
-from threading import Thread
+from threading import Thread, Lock
+from pubsub import pub
+
+from src.navigation_mode import NavigationMode
+from src.nav.course import ObjectCourse
+
+mutex = Lock()
 
 class Event(ABC, Thread):
     """Base event type"""
@@ -9,7 +15,62 @@ class Event(ABC, Thread):
         """Creates a new event"""
         Thread.__init__()
 
+        self.is_active = True
+        self.read_interval()
+
+        self.course = ObjectCourse()
+        
+        pub.subscribe(self.switch_mode, "set nav mode")
+
     @abstractmethod
-    def add_waypoint(self, obj):
-        """Adds waypoint at object"""
+    def run(self):
+        """Runs event thread"""
+
+    @abstractmethod
+    def read_interval(self):
+        """Reads update interval from config"""
         pass
+
+    def switch_mode(self, mode):
+        """
+        Changes the navigation mode
+        Inputs:
+            mode -- navigation mode to switch to
+        """
+        if mode is NavigationMode.AUTONOMOUS:
+            self.enable()
+        else:
+            self.disable()
+
+    def enable(self):
+        """Enables autonomous navigation"""
+        self.is_active = True
+
+    def disable(self):
+        """
+        Disable autonomous navigation
+        Side Effects:
+            course -- clears course
+        """   
+        self.is_active = False
+        self.course.clear()
+
+    def add_object(self, obj):
+        """
+        Adds waypoint at object
+        Side Effects:
+            course -- adds object to course
+        """
+        mutex.acquire()
+        self.course.add_obj(obj)
+        mutex.release()
+
+    def clear_course(self):
+        """
+        Clears course of all obejcts 
+        Side Effects:
+            course -- clears course
+        """
+        mutex.acquire()
+        self.course.clear()
+        mutex.release()
