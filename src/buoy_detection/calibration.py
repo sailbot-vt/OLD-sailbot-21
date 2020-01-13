208ff6fc5b34ef149a14b9ec2a2f40da51dff77e
Evan Allen <evanallen@vt.edu>
2020-01-13 18:13:22 -0500

    Finished refactoring calibration.py and added a ton of documentation about how it works.

--- src/buoy_detection/calibration.py
+++ src/buoy_detection/calibration.py
@@ -1,3 +1,90 @@
+"""
+calibration.py
+Contains a class, Calibrator, that can compute calibration data for two stereo cameras based on a set of
+image pairs from each of them.
+
+# REQUIREMENTS FOR CALIBRATION:
+-   A stereo set of images from a pair of LEFT/RIGHT cameras, which are each as close to parallel as possible
+    (important!).
+-   Each image must contain a chessboard calibration pattern visible to BOTH cameras.
+-   The images must be stored in `base_path/LEFT` and `base_path/RIGHT`, respectively, where base_path is some
+    path provided to the Calibrator class. Each image in a LEFT/RIGHT pair should have the same base name so that
+    the Calibrator can associate them with one another.
+-   For accurate calibration, the chessboard pattern must be printed extremely accurately
+    (laser-printer recommended).
+
+# BACKGROUND:
+-   Stereo cameras are useful because they allow us to judge depth by comparing the locations of an object in images
+    taken simultaneously by two side-by-side cameras, just like how we can judge depth with our two eyes.
+
+-   To calculate this depth, though, we need to know exactly how the two cameras are:
+    1. Positioned (i.e., where they ARE) relative to each other, and how they're
+    2. Oriented (i.e., where they're LOOKING) relative to each other.
+        DEFN: This data is called the stereo cameras' *EXTRINSIC PARAMETERS*.
+        DEFN: Extrinsic parameters are calculated through *STEREO CALIBRATION*.
+
+-   In addition, each camera has certain unique properties related to its focal length and distortion that we must
+    know.
+        DEFN: These properties are called *INTRINSIC PARAMETERS*, and are specific to each INDIVIDUAL camera.
+
+-   Consider a point viewed by a camera.
+        DEFN: Its 3D coordinates in the real world are referred to as an *OBJECT POINT*.
+        DEFN: Its 2D coordinates projected onto the image plane are referred to as an *IMAGE POINT*.
+
+-   We use a well-defined pattern like a chessboard to determine these intrinsic and extrinsic parameters by
+    associating object points (which we know IN ADVANCE, since we know the size of the chessboard) with image points
+    in each camera, and comparing the two sets of image points between the cameras.
+    -   To elaborate: we know the object points of the chessboard in advance because we _set our coordinate system_
+        such that the chessboard is the XY-plane with the origin at one corner and the axes along the edges.
+        With this view, it's like the chessboard is stationary and we're moving the cameras (even if we're not).
+
+-   When we want to compare the images from each camera to determine the depth of a point in the image, it's important
+    that the image planes of each camera lie in the same plane (which we achieve if the cameras are EXACTLY parallel).
+    This is difficult to do in real life, though, so we can slightly transform the images so that it's _as if_ they
+    were taken from two cameras that were exactly parallel.
+        DEFN: This process of transforming images so they seem like they were taken from parallel cameras is called
+              *STEREO RECTIFICATION*.
+              Further reading:  http://people.scs.carleton.ca/~c_shu/Courses/comp4900d/notes/rectification.pdf
+
+# HOW IT WORKS:
+    I. SETUP & DATA COLLECTION
+        1.  Set up the cameras next to each other so that they are _as parallel as possible_ and measure the
+            physical distance between them in meters (this is called the BASELINE, which is used in other files).
+            DO NOT MOVE THE CAMERAS! If you do, you'll have to calibrate again.
+        2.  Take a bunch of pictures of a chessboard calibration pattern with both the left and right cameras
+            at the same time, storing each pair in the folders specified above.
+
+    II. CORNER IDENTIFICATION
+        3.  Once you have the data, the Calibrator will enumerate through each set of images trying to identify
+            the image points of each of the chessboard's internal corners.
+            a.  If the Calibrator cannot identify all the expected corners in an image, it (and its corresponding pair
+                image from the other camera) are removed.
+        4.  Then the Calibrator refines its image points to a sub-pixel level using cv2.cornerSubPix.
+
+    III. INDIVIDUAL CAMERA CALIBRATION
+        5.  The Calibrator then determines intrinsic parameter matrices for the left and right cameras (as well
+            as their distortion coefficients) using these image point / object point correspondence data.
+
+    IV. STEREO CAMERA CALIBRATION
+        6.  Then, the Calibrator determines the rotation matrix and translation vector that transform points from
+            the first camera's view to the second's. These form the extrinsic parameters.
+
+    V. STEREO RECTIFICATION
+        7.  Using the image point / object point data as well as the intrinsic parameters of each camera, the Calibrator
+            calculates a set of RECTIFICATION and PROJECTION matrices that allow us to transform image points viewed by
+            either camera into image points on a common plane, which allows us to take disparity measurements
+            (how far apart two corresponding image points are between cameras) and consequently depth measurements.
+            a.  Because this process provides us matrices to transform images from both cameras, it also gives us
+                data on which part of those transformed images are actually useful (called REGIONS OF INTEREST, or ROI).
+            b.  In addition, it also provides a Q-MATRIX, which converts points in image space (along with a disparity)
+                to 3D points in physical space.
+
+    VI. SAVING
+        8.  Finally, the data is saved. Calibration data that can be used for depth calculation is stored in
+            "stereo_calibration.npz" at some desired location, and the projection/rectification matrices themselves
+            are stored in "projection_matrices.npz" at some desired location.
+"""
+
 import glob
 import os
 import numpy as np
@@ -15,13 +102,6 @@ class Calibrator:
                  draw_image=True):
         """
         Class that contains machinery for doing stereo camera calibration!
-        Requires a stereo set of images from a pair of LEFT/RIGHT cameras, which are each PARALLEL (extremely
-        important!).
-        Each image must contain a chessboard calibration pattern visible to BOTH cameras. The parameters of the
-        chessboard pattern must be entered into this class through `square_size` and `grid_size` (see below).
-        The images must be stored in `base_path/LEFT` and `base_path/RIGHT`, respectively.
-        For accurate calibration, the chessboard pattern must be printed extremely accurately
-        (laser-printer recommended).
         To run the calibration process and save the results, instantiate this class and then use the `run_calibration`
         method.
         :param square_size: The real size of each chessboard square, in meters.
@@ -62,7 +142,6 @@ class Calibrator:
         Saves final result matrices to out_file1 and out_file2.
         :param out_file1: The path to save the stereo calibration matrices.
         :param out_file2: The path to save the projection & rectification matrices.
-        TODO: Make these names/descriptions better.
         """
 
         (object_points, left_image_points, camera_size) = \
@@ -192,9 +271,18 @@ class Calibrator:
         print("Done")
 
         print("Starting stereo rectification...")
-        # Rectification matrices are the 3x3 rotation matrices for each of the two cameras
-        # Project matrices are the new rectified coordinate systems for each of the two cameras
-        # Q is the disparity to depth mapping
+        # Rectification matrices are the 3x3 matrices that rotate them onto a common plane (so that their epipolar
+        # lines are parallel and disparities only occur horizontally).
+        #
+        # Project matrices are the new rectified coordinate systems for each of the two cameras that map points
+        # in 3D space to points in 2D image space.
+        #
+        # Q is the disparity to depth mapping in homogeneous coordinates - essentially allows you to
+        # take the 2D coordinates of a pixel and its disparity, and find its 3D location.
+        #   (input: 4-vector of the form [u, v, d, 1]
+        #       where u, v are pixel coordinates and d is disparity;
+        #   output: 4-vector of the form [x, y, z, w] where
+        #       x, y, and z are 3D coordinates and w scales them appropriately (see 'Homogeneous coordinates')
         (leftRectification, rightRectification, left_projection, right_projection,
          Q_matrix, left_roi, right_roi) = \
             cv2.stereoRectify(
