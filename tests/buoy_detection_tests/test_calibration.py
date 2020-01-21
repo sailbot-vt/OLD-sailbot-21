77c79ded181fd7c6672332210419518157f6b718
Evan Allen <evanallen@vt.edu>
2020-01-20 19:10:31 -0500

    Added __init__.py file to buoy_detection_tests folder so it's discoverable and created first test for the calibration file.

---
+++ tests/buoy_detection_tests/test_calibration.py
@@ -0,0 +1,29 @@
+import unittest
+try:
+    from unittest.mock import MagicMock, patch, call
+except ImportError:
+    from mock import MagicMock, patch, call
+
+import src.buoy_detection.calibration as calibration
+import src.buoy_detection.config_reader as config_reader
+
+
+class CalibrationTests(unittest.TestCase):
+    """Tests the methods in buoy_detection/calibration.py."""
+
+    def setUp(self):
+        self.config_filename = "test_config.yaml"
+        self.calibration_config = config_reader.get_calibration_config(self.config_filename)
+
+    @patch("src.buoy_detection.calibration.Calibrator")
+    def test_load_config_and_run(self, mock_calibrator):
+        calibration.load_config_and_run(self.config_filename)
+
+        # Test if the Calibrator is created with the right configuration.
+        mock_calibrator.assert_called_once_with(self.calibration_config)
+
+        # Test if the run_calibration method is called with the correct output file-names.
+        mock_calibrator.assert_has_calls([call().run_calibration("stereo_calibration.npz", "projection_matrices.npz")])
+
+
+
