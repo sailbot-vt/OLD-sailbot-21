21b877c200d7a8a4b8b7c851254c6249922be155
evanq126 <evanq126@vt.edu>
2020-01-31 16:08:19 -0500

    fixed broadcaster and tests

--- tests/broadcaster_tests/test_broadcaster.py
+++ tests/broadcaster_tests/test_broadcaster.py
@@ -1,5 +1,7 @@
 import os
 import unittest
+import parse
+
 try:
     from unittest.mock import patch
 except ImportError:
@@ -25,33 +27,36 @@ class BroadcasterTests(unittest.TestCase):
             broadcaster_type=BroadcasterType.FileWriter, 
             filename=self.path + "/broadcast_test.log")
         self.data = {
-            1 : "test",
-            2 : "test2",
-            3 : "test3"
+            "1" : "test1",
+            "2" : "test2",
+            "3" : "test3"
         }
 
-    def test_update_data(self):
+    @patch('src.broadcaster.broadcaster.pub', autospec=True)
+    def test_update_data(self, mock_pub):
         """ Tests update data for broadcasters """
-        self.assertEqual(self.testable.data, None)
-        self.assertEqual(self.messenger.data, None)
-        self.assertEqual(self.filewriter.data, None)
-
         self.testable.publish_dictionary(self.data)
         self.messenger.publish_dictionary(self.data)
         self.filewriter.publish_dictionary(self.data)
 
+        # Local storage 
         self.assertEqual(self.testable.data, self.data)
-        self.assertEqual(self.messenger.data, self.data)
-        self.assertEqual(self.filewriter.data, self.data)
 
-    @patch('src.broadcaster.broadcaster.open')
-    @patch('src.broadcaster.broadcaster.pub')
-    def test_read_data(self, mock_open, mock_pub):
-        """ Tests read data """
-        self.testable.publish_dictionary(self.data)
-        self.messenger.publish_dictionary(self.data)
-        self.filewriter.publish_dictionary(self.data)
+        # Pubsub
+        mock_pub.sendMessage.assert_any_call(topicName="1", msgData="test1")
+        mock_pub.sendMessage.assert_any_call(topicName="2", msgData="test2")
+        mock_pub.sendMessage.assert_any_call(topicName="3", msgData="test3")
 
-        self.assertEqual(self.testable.publish_key(key=1), "test")
-        self.assertEqual(self.messenger.publish_key(key=2), "test2")
-        self.assertEqual(self.filewriter.publish_key(key=3), "test3")
+        # File
+        f = open(self.filewriter.filename, "r")
+        lines = f.readlines() 
+        # Since dictionary keys may not be published in order,
+        # we have to store the key-value pairs from file to temp dictionary
+        # for testing purposes.
+        line_format = "[{0}]\t\t[Requested: {1} -- Data: {2}]\n"
+
+        res_dict = dict()
+        for line in lines:
+            parsed = parse.parse(line_format, line)
+            res_dict[parsed[1]] = parsed[2]
+        self.assertEqual(self.data, res_dict)
