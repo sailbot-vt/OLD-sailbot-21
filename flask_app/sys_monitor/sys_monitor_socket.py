88c80b449d83f0549289876498a28b660ca8b684
joser12345678 <56372006+joser12345678@users.noreply.github.com>
2020-02-04 17:46:03 -0500

    Update sys_monitor_socket.py
    Added psutil functionality

--- flask_app/sys_monitor/sys_monitor_socket.py
+++ flask_app/sys_monitor/sys_monitor_socket.py
@@ -1,3 +1,4 @@
+import psutil
 from flask_socketio import Namespace, emit
 
 
@@ -5,3 +6,25 @@ class SysMonitorSocket(Namespace):
 
     def __init__(self, Namespace=None):
         super().__init__(Namespace)
+
+    def cpu_usage(self):
+	"""
+	Returns:
+	A string containing the cpu usage percent.
+	"""
+        return psutil.cpu_percent(interval=1)
+
+     def cpu_temperature(self):
+	"""
+	Returns:
+	A string that returns hardware temperatures in celcius.
+	For Farenheit, specify fahrenheit as true.
+	"""
+	return psutil.sensors_temperatures(fahrenheit=False)
+
+     def memory_usage(self):
+	"""
+	Returns:
+	A string that contains information on total and available memory, as 		well as other memory info.
+	"""
+	return psutil.virtual_memory()
