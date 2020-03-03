b887d7695a756c7ce5c3cc5910b3e9c3315234c6
adityapoudyal <adityapoudyal@vt.edu>
2020-03-03 00:21:42 -0500

    Updated captain class with new methods

--- src/autonomy/nav/captain.py
+++ src/autonomy/nav/captain.py
@@ -4,10 +4,10 @@ from time import sleep
 from pubsub import pub
 
 from src.navigation_mode import NavigationMode
-from src.nav.tack_points import place_tacks
-from src.nav.config_reader import read_interval
-from src.nav.course import Course, Path
-from src.autopilot.autopilot import Autopilot
+from src.autonomy.nav.tack_points import place_tacks
+from src.autonomy.nav.config_reader import read_interval
+from src.autonomy.nav.course import Course, Path, ObjectCourse
+from src.autonomy.autopilot.autopilot import Autopilot
 
 
 mutex = Lock()
@@ -16,14 +16,17 @@ mutex = Lock()
 class Captain(Thread):
     """Thread to manage autonomous navigation"""
 
-    def __init__(self, boat, world, buoy_detection=False):
+    def __init__(self, boat, wind, objectives, buoy_detection=False):
         """Builds a new captain thread."""
         super().__init__()
         self.boat = boat
-        self.world = world
+        self.wind = wind
+        self.objectives = objectives
+        self.object_course = None
+        self.create_course(self.objectives)
+
         self.is_active = True
         self.nav_interval = read_interval()
-        self.autopilot = Autopilot(boat, world)
         if buoy_detection:
             self.course = Path()
         else:
@@ -34,6 +37,8 @@ class Captain(Thread):
         """Runs the captain thread"""
         print("Started captain thread")
         while True:
+            self.update_objectives()
+
 #            if self.is_active:
             if 0:
                 mutex.acquire()
@@ -44,7 +49,7 @@ class Captain(Thread):
                 if len(self.autopilot.route) is 0:
                     leg = course.next_leg()
 
-                self.autopilot.new_route(place_tacks(self.boat.current_position, leg[1], self.boat, self.world.wind))
+                self.autopilot.new_route(place_talecks(self.boat.current_position, leg[1], self.boat, self.world.wind))
                 self.autopilot.activate()
 
                 sleep(self.nav_interval)
@@ -77,3 +82,14 @@ class Captain(Thread):
         mutex.acquire()
         self.course = Course()
         mutex.release()
+
+    def create_course(self, objectives):
+        """ Creates the object course using objectives"""
+        self.object_course = ObjectCourse(objectives)
+
+    def update_objectives(self):
+        """ Updates the object course based on which objectives have been completed"""
+        self.object_course = self.object_course.update_objects(self.objectives)
+
+    def set_tacks(self):
+        place_tacks(self.boat.current_position, leg[1], self.boat, self.wind)
